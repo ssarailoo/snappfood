5520ed52aca0147a35af198b9184d052c7b94406
ssarailoo <ssarailoo@yahoo.com>
2023-11-15 02:25:55 +0330

    feat: add view for manage carts of restaurant

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('dashboard');
+
+        return view('dashboard', [
+            'carts' => Auth::user()->restaurant->carts()
+//                ->whereNotNull('status')
+                ->where('status', '!=', 'received')
+                ->get(),
+        ]);
     }
 
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,4 @@
+@php use App\Models\Address\Address;use Illuminate\Support\Facades\Auth; @endphp
 <x-app-layout>
 
     @if(session('success'))
@@ -19,6 +20,82 @@
                     {{ __("You're logged in!") }}
                 </div>
             </div>
-        </div>
-    </div>
+            @if(Auth::user()->restaurant==!null)
+                <div>
+                    <div class="sm:p-8 bg-white shadow sm:rounded-lg p-6">
+                        <div class="relative overflow-x-auto">
+                            <table class="w-full text-sm text-left text-pink-500 dark:text-pink-500">
+                                <thead
+                                    class="text-xs text-pink-500 uppercase bg-white dark:bg-white dark:text-pink-500">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        ID
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Customer's Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Customer's Address
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Customer's Phone Number
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Foods
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Status
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Action
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Action
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($carts as $key=> $cart)
+                                    <tr class="bg-white dark:bg-white">
+
+
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-pink-500 whitespace-nowrap dark:text-pink-500">
+                                            <a href=""> {{$key+1}}</a>
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{$cart->user->name}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{Address::query()->find( $cart->user->current_address)->address}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{$cart->user->phone_number}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            @foreach($cart->foods->unique('id') as $food)
+                                                <p class="text-gray-700 text-base">
+                                                    {{$food->name}}           {{$food->price}}
+                                                    * {{(int)$cart->cartFoods->where('food_id', $food->id)->sum('food_count')}}
+                                                </p>
+                                            @endforeach
+                                        </td>
+
+                                        <td class="px-6 py-4">
+                                            {{$cart->status}}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                          #
+                                        </td>
+                                        <td class="px-6 py-4">
+                                         #
+                                        </td>
+
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        @endif
+                    </div>
+                </div>
 </x-app-layout>
