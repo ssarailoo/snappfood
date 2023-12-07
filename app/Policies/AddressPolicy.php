9c0c8000e616c2e921bfe91a4dd239681f11cc8e
ssarailoo <ssarailoo@yahoo.com>
2023-12-07 20:17:14 +0330

    refactor: refactor AddressPolicy

--- app/Policies/AddressPolicy.php
+++ app/Policies/AddressPolicy.php
@@ -4,14 +4,15 @@ namespace App\Policies;
 
 use App\Models\Address\Address;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class AddressPolicy
 {
     /**
      * Create a new policy instance.
      */
-    public function myAddress(User $user, Address $address): bool
+    public function myAddress(User $user, Address $address)
     {
-        return $user->addresses->contains($address);
+        return $user->addresses->contains($address) ? Response::allow() : Response::deny('This Address does not belong to you')->withStatus(403);
     }
 }
